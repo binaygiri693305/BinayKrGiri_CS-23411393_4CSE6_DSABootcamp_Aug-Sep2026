eb5e030253a827ba11fb7e9b00a889be463ed5f3
Harjit Singh Bhadauriya <163171644+nonehxrry@users.noreply.github.com>
2026-09-01 14:14:59 +0530

    Create 930. Binary Subarrays With Sum.java

---
+++ Day-14/930. Binary Subarrays With Sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        int sum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+
+            sum += num;
+
+            int required = sum - goal;
+
+            count += map.getOrDefault(required, 0);
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+}
